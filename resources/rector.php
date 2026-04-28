98331b28188d91af785fc0f808d12daac5bab752
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-04-28 15:33:39 +0600

    wild composer

--- resources/rector.php
+++ resources/rector.php
@@ -5,10 +5,49 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\ValueObject\PhpVersion;
 
-return RectorConfig::configure()
-    ->withPaths([getcwd() . '/src'])
+function resolveRectorPhpVersion(): ?int
+{
+    $reflection = new ReflectionClass(PhpVersion::class);
+    $constants = $reflection->getConstants();
+    $current = (PHP_MAJOR_VERSION * 10) + PHP_MINOR_VERSION;
+    $candidates = [];
+
+    foreach ($constants as $name => $value) {
+        if (! is_int($value) || ! is_string($name) || ! str_starts_with($name, 'PHP_')) {
+            continue;
+        }
+
+        $suffix = substr($name, 4);
+
+        if ($suffix === false || $suffix === '' || ! ctype_digit($suffix)) {
+            continue;
+        }
+
+        $versionId = (int) $suffix;
+
+        if ($versionId <= $current) {
+            $candidates[$versionId] = $value;
+        }
+    }
+
+    if ($candidates === []) {
+        return null;
+    }
+
+    ksort($candidates);
+
+    return end($candidates) ?: null;
+}
+
+$config = RectorConfig::configure()
+    ->withPaths([getcwd().'/src'])
     ->withPreparedSets(deadCode: true)
-    ->withPhpVersion(
-        constant(PhpVersion::class . '::PHP_' . PHP_MAJOR_VERSION . PHP_MINOR_VERSION),
-    )
     ->withPhpSets();
+
+$resolvedPhpVersion = resolveRectorPhpVersion();
+
+if (is_int($resolvedPhpVersion)) {
+    $config = $config->withPhpVersion($resolvedPhpVersion);
+}
+
+return $config;
